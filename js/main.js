30c48f2e8616572719607caa499ca2316012d848
Sydney C Doemel Doemel <sydneycdoemeldoemel@sydneys-mbp.attlocal.net>
2022-08-09 20:47:16 -0400

    added codewars

--- js/main.js
+++ js/main.js
@@ -44,3 +44,47 @@ console.log(replaceEvens(["Max","Baseball","Reboot","Goku","Trucks","Rodger"]))
 //Expected output
 //Given arr == ["Max","Baseball","Reboot","Goku","Trucks","Rodger"]
 //Output arr == ["even index","Baseball","even index","Goku","even index","Rodger"]
+
+
+
+
+
+
+
+
+
+// DESCRIPTION:
+// Create a function with two arguments that will return an array of the first (n) multiples of (x).
+// Assume both the given number and the number of times to count will be positive numbers greater than 0.
+// Return the results as an array (or list in Python, Haskell or Elixir).
+// Examples:
+// countBy(1,10) === [1,2,3,4,5,6,7,8,9,10]
+// countBy(2,5) === [2,4,6,8,10]
+
+function countBy(x, n) {
+  let z = [];
+  let count = 1;
+  while (count < n+1){
+    z.push(x*count);
+    count ++
+  }
+  
+  return z;
+}
+
+
+// DESCRIPTION:
+// You get an array of numbers, return the sum of all of the positives ones.
+// Example [1,-4,7,12] => 1 + 7 + 12 = 20
+// Note: if there is nothing to sum, the sum is default to 0.
+
+
+function positiveSum(arr) {
+  let count = 0
+  for(let each of arr){
+    if (each >=0){
+      count += each
+    }
+  }
+  return count
+}
